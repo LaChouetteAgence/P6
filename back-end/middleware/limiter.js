4fca310db08524cb0aa7e2565c2fd9dfa17f5d19
NordineS <97751426+LaChouetteAgence@users.noreply.github.com>
2022-06-15 13:47:47 +0200

    Delete back-end/middleware directory

--- back-end/middleware/limiter.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require("express-rate-limit");
-
-const max = rateLimit({
-  windowMs: 3 * 60 * 1000, // délai en ms / 3 minutes
-  max: 3, // nombre de tentatives autorisées
-});
-
-// EXPORT
-module.exports = { max };
